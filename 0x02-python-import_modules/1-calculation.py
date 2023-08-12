dd706a36c48e09f4a61ac57680e9f245dd752663
Musa Yahaya <musaybappah@gmail.com>
2023-08-11 23:25:46 -0700

    import arithmetic operation functions from a module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-import importlib
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
 
-calc_module = importlib.import_module("calculator_1")
-
-add = calc_module.add
-sub = calc_module.sub
-mul = calc_module.mul
-div = calc_module.div
-
-a = 10
-b = 5
-
-print(f"{a} + {b} = {add(a, b)}")
-print(f"{a} - {b} = {sub(a, b)}")
-print(f"{a} * {b} = {mul(a, b)}")
-print(f"{a} / {b} = {div(a, b)}")
+    a = 10
+    b = 5
+    
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
