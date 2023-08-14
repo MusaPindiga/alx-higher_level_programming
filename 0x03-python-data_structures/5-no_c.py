7f8dd6c82410693098fd9029c5b9e1ee70015d12
Musa Yahaya <musaybappah@gmail.com>
2023-08-14 16:25:33 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,6 @@ def no_c(my_string):
 
     characters_to_remove = 'Cc'
 
-    new_string = ''.join(char for char in my_string if char not in characters_to_remove)
-    return new_string
-
-
+    new_string = ''.join(char for char in my_string
+                         if char not in characters_to_remove)
+    return new_strig
