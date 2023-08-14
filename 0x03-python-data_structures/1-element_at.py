eeadf2d2d620a688e266a08853c1cefb0ec77093
Musa Yahaya <musaybappah@gmail.com>
2023-08-14 10:21:59 -0700

    fix PEP8

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idx):
+#!/usr/bin/python3
 
+def element_at(my_list, idx):
+    """
+    Get the element at the specified index in the list.
+
+    Args:
+        my_list (list): The input list.
+        idx (int): The index of the element to retrieve.
+
+    Returns:
+        The element at the specified index, or None if the index is out of bounds.
+    """
     size = len(my_list)
     i = 0
 
-    if idx < 0 or idx > size:
-        return
+    if idx < 0 or idx >= size:
+        return None
 
     while i < size:
-
         if i == idx:
             return my_list[i]
-
         i += 1
+
 
