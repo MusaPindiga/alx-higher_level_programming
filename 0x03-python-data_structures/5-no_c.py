04063da96c71420f6bbda3187a847020a26e4a8c
Musa Yahaya <musaybappah@gmail.com>
2023-08-14 14:13:01 -0700

    remove a character from string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+
+    characters_to_remove = 'Cc'
+
+    new_string = ''.join(char for char in my_string if char not in characters_to_remove)
+    return new_string
+
+
