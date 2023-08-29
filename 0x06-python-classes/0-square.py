09c65f7751b8dcbd21253a9e957df2645a306e0e
Musa <musaybappah@gmail.com>
2023-08-29 10:32:35 -0700

    a simple class template

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A simple class representing a square.
+
+    This class doesn't contain any attributes or methods yet.
+    It serves as a template for creating square objects.
+
+    Attributes:
+        No attributes are defined in this class.
+
+    Methods:
+        No methods are defined in this class.
+    """
+    pass
